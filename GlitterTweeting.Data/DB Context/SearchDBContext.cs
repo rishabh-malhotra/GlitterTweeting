010d46d8eaddc711fe08ed1a57750637e064c8aa
rishabh-malhotra <rishabhmalhotra110796@gmail.com>
2018-12-29 18:21:23 +0530

    minor update

--- GlitterTweeting.Data/DB Context/SearchDBContext.cs
+++ GlitterTweeting.Data/DB Context/SearchDBContext.cs
@@ -5,9 +5,9 @@ using System.Linq;
 
 namespace GlitterTweeting.Data.DB_Context
 {
-    public class SearchDBContext:IDisposable
+    public class SearchDBContext : IDisposable
     {
-        glitterEntities DBContext;
+        glitterEntities DBContext=new glitterEntities();
         TweetDBContext tbc;
         public SearchDBContext()
         {
@@ -20,14 +20,16 @@ namespace GlitterTweeting.Data.DB_Context
         /// <param name="searchString"></param>
         /// <param name="UserId"></param>
         /// <returns></returns>
-        public IList<SearchDTO> GetAllUsers(string searchString,Guid UserId)
+        public IList<SearchDTO> GetAllUsers(string searchString, Guid UserId)
         {
-            if (searchString != null && searchString!="")
+           
+            if (searchString != null && searchString != "")
             {
 
                 IList<SearchDTO> resultList = new List<SearchDTO>();
                 SearchDTO getAllUsers;
-                IList<User> user = DBContext.User.Where(ds => (ds.FirstName.Contains(searchString) || ds.LastName.Contains(searchString)) && (ds.ID!=UserId )).ToList();
+                IList<User> user = new List<User>();
+                user = DBContext.User.Where(ds => (ds.FirstName.Contains(searchString) || ds.LastName.Contains(searchString)) && (ds.ID != UserId)).ToList();
                 if (user.Count > 0)
                 {
                     foreach (var item in user)
@@ -41,10 +43,10 @@ namespace GlitterTweeting.Data.DB_Context
                         Follow f = DBContext.Follow.Where(fo => (fo.Follower_UserID == UserId) && (fo.Followed_UserID == getAllUsers.UserId)).FirstOrDefault();
                         if (f != null)
                         {
-                            getAllUsers.isFollowed =true;
+                            getAllUsers.isFollowed = true;
                         }
                         else
-                            getAllUsers.isFollowed =false;
+                            getAllUsers.isFollowed = false;
 
                         resultList.Add(getAllUsers);
                     }
@@ -57,7 +59,7 @@ namespace GlitterTweeting.Data.DB_Context
                 }
             }
             else return null;
-            }
+        }
 
 
 
@@ -67,35 +69,37 @@ namespace GlitterTweeting.Data.DB_Context
         /// <param name="searchString"></param>
         /// <param name="userId"></param>
         /// <returns></returns>
-        public IList<SearchDTO> GetAllHashTag(string searchString,Guid userId)
+        public IList<SearchDTO> GetAllHashTag(string searchString, Guid userId)
         {
-            if (searchString != null && searchString!="")
+            if (searchString != null && searchString != "")
             {
-                IList<Tag> tag = DBContext.Tag.Where(de => de.TagName.Contains(searchString)).ToList();
-                IList<SearchDTO> resultList = new List<SearchDTO>();
+                
+                    IList<Tag> tag = DBContext.Tag.Where(de => de.TagName.Contains(searchString)).ToList();
+                    IList<SearchDTO> resultList = new List<SearchDTO>();
 
-                SearchDTO getAllTags;
-                if (tag.Count > 0)
-                {
-                    foreach (var item in tag)
+                    SearchDTO getAllTags;
+                    if (tag.Count > 0)
                     {
-                        getAllTags = new SearchDTO();
-                        tbc.updateSearchCount(item);
-                        IList<Tweet> tweet = DBContext.Tweet.Where(dr => dr.ID == item.TweetID).ToList();
-                        foreach (var item1 in tweet)
+                        foreach (var item in tag)
                         {
-                            User user1 = DBContext.User.Where(dw => dw.ID == item1.UserID).FirstOrDefault();
-                            getAllTags.Message = item1.Message;
-                            getAllTags.CreatedAt = item1.CreatedAt;
-                            getAllTags.UserName = user1.FirstName + user1.LastName;
-                            
-                        }
-                        resultList.Add(getAllTags);
-                    }
-                    return resultList;
-                }
+                            getAllTags = new SearchDTO();
+                            tbc.updateSearchCount(item);
+                            IList<Tweet> tweet = DBContext.Tweet.Where(dr => dr.ID == item.TweetID).ToList();
+                            foreach (var item1 in tweet)
+                            {
+                                User user1 = DBContext.User.Where(dw => dw.ID == item1.UserID).FirstOrDefault();
+                                getAllTags.Message = item1.Message;
+                                getAllTags.CreatedAt = item1.CreatedAt;
+                                getAllTags.UserName = user1.FirstName + user1.LastName;
 
-                else return null;
+                            }
+                            resultList.Add(getAllTags);
+                        }
+                        return resultList;
+                    }
+
+                    else return null;
+                
             }
             else return null;
         }
